b47980a2fcc756b71058c54ff54549cb3b872240
Louis de Beaumont <louis.de-beaumont14@imperial.ac.uk>
2017-02-18 21:52:03 +0000

    Clean up website file structure (remove Home)

--- js/router.js
+++ js/router.js
@@ -1,14 +1,8 @@
 DribbleApp.Router = Backbone.Router.extend({
 
   routes: {
-    'home': 'home',
     'search': 'search',
-    '*path': 'home'
-  },
-
-  home: function() {
-    var view = new DribbleApp.Views.Home();
-    $('#main').html(view.render().el);
+    '*path': 'search'
   },
 
   search: function() {
--- js/views/home.js
+++
@@ -1,9 +0,0 @@
-DribbleApp.Views.Home = Backbone.View.extend({
-
-  initialize: function(options) {},
-
-  render: function() {
-    this.$el.html('Home page');
-    return this;
-  }
-});
